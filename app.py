d04683cbbabf54b46c8deb5af599a025d6148f4a
Nadine Lessio <idoru00@gmail.com>
2016-12-10 23:31:26 -0500

    making the front end.

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def main():
 
 if __name__ == '__main__': # If we're executing this app from the command line
     #app.run("127.0.0.1", port = 3000, debug=True, use_reloader=False)
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=False, use_reloader=False)
